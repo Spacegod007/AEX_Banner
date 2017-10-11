cff0ec8a4bebe5934d7a7e530ee5d4d4f1b4a7d3
Spacegod007 <spacegod007@outlook.com>
2017-10-11 14:21:57 +0200

    Basic RMI implementation (not working)

--- src/logic/IFonds.java
+++ src/logic/IFonds.java
@@ -1,9 +1,11 @@
 package logic;
 
+import java.io.Serializable;
+
 /**
  * An interface that shows methods that are required in a fonts class
  */
-public interface IFonds
+public interface IFonds extends Serializable
 {
     String getName();
 
