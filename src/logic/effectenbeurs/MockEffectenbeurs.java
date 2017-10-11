dff7038a1bbb6aca9aa7c0cdcd8f9feafdb53803
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2017-10-11 11:23:45 +0200

    Started on effectenbeurs

--- src/logic/effectenbeurs/MockEffectenbeurs.java
+++ src/logic/effectenbeurs/MockEffectenbeurs.java
@@ -2,6 +2,7 @@ package logic.effectenbeurs;
 
 import logic.IFonds;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +10,15 @@ import java.util.List;
  */
 public class MockEffectenbeurs implements IEffectenbeurs
 {
+    private List<IFonds> fondsen;
+
+    public MockEffectenbeurs()
+    {
+        fondsen = new ArrayList<IFonds>(){
+            
+        };
+    }
+
     @Override
     public List<IFonds> getKoersen()
     {
